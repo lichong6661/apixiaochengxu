eaf482d2f6fbe0b6a3cf85f263a0e65dbcc8160f
lichongGit <lichong_1@qq.com>
2019-01-17 09:41:58 +0800

    ee

--- application/libExcep/base/ExceptionHandler.php
+++ application/libExcep/base/ExceptionHandler.php
@@ -60,7 +60,8 @@ class ExceptionHandler extends Handle
         return json($data,$this->code,[
             'Access-Control-Allow-Origin'=>'*',
             'Access-Control-Allow-Methods'=>'GET, POST, OPTIONS, DELETE',
-            'Access-Control-Allow-Headers'=>'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding'
+            //'Access-Control-Allow-Headers'=>'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding'
+            'Access-Control-Allow-Headers'=>'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         ]);
     }
 
